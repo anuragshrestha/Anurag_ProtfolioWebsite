01e19db7727e785c9811c4b93a0e12ca5eb74f8e
Anurag Shrestha <anuragshrestha16@gmail.com>
2025-06-05 15:57:20 -0600

    hover animation

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Layout from './component/Layout/Layout';
 
 import './App.scss';
 import { Routes, Route } from 'react-router-dom';
+import About from './component/About/About';
 
 function App() {
   return (
    <Routes>
     <Route path='/' element={<Layout/>}>
       <Route index element={<Home/>} />
+      <Route path='/about' element={<About/>}/>
     </Route>
   
    </Routes>
