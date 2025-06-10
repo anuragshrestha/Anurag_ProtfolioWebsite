e8156c0f8ca761ecd625bdbdc8ba69da5cb984ff
Anurag Shrestha <anuragshrestha16@gmail.com>
2025-06-09 23:32:50 -0600

    updated code

--- src/component/Contact/Contact.js
+++ src/component/Contact/Contact.js
@@ -3,45 +3,33 @@ import './Contact.scss'
 import Animated from '../AnimatedLayers/Animated'
 import { useState, useEffect, useRef } from 'react'
 import emailjs from '@emailjs/browser'
+import { MapContainer, Marker, TileLayer } from 'react-leaflet'
 
 const Contact = () => {
-  const [letterClass, setLetterClass] = useState('text-animate');
-  const refForm = useRef();
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const refForm = useRef()
 
   useEffect(() => {
     const timeoutId = setTimeout(() => {
       setLetterClass('text-animate-hover')
     }, 3000)
-
     return () => clearTimeout(timeoutId)
   }, [])
 
   const sendEmail = (e) => {
     e.preventDefault()
-   
 
-    emailjs.sendForm(
-    'service_dvke4al',
-    'template_fkfwn9y',
-    refForm.current,
-    'vIUZN6pg9vnvlbJ_0'
-  );
-
-   emailjs.sendForm(
-    'service_dvke4al',
-    'template_4mnu8u9',
-     refForm.current,
-    'vIUZN6pg9vnvlbJ_0'
-   )
-   .then(
-    () => {
-        alert('Message send successfully.')
-        window.location.reload(false)
-    },
-    () => {
-        alert('Failed to send message. Please try again.')
-    }
-   )
+    emailjs
+      .sendForm('service_dvke4al', 'template_fkfwn9y', refForm.current, 'vIUZN6pg9vnvlbJ_0')
+      .then(
+        () => {
+          alert('Message sent successfully.')
+          window.location.reload(false)
+        },
+        () => {
+          alert('Failed to send message. Please try again.')
+        }
+      )
   }
 
   return (
@@ -56,30 +44,44 @@ const Contact = () => {
             />
           </h1>
           <p>
-            I am interested in Software Engineering roles focusing mainly on
-            Mobile App development and Backend development.
+            I am interested in Software Engineering roles focusing mainly on Mobile App development and Backend development.
           </p>
-          <div className='contact-form'>
+          <div className="contact-form">
             <form ref={refForm} onSubmit={sendEmail}>
-            <ul>
-              <li className="half">
-                <input type="text" name="name" placeholder="Name" required />
-              </li>
-              <li className='half'>
-                <input type='email' name='email'  placeholder='Email' required/>
-              </li>
-              <li>
-                <input type='text' name='subject' placeholder='Subject' required/>
-              </li>
-              <li>
-                <textarea placeholder='Message' name='message' required/>
-              </li>
-              <li>
-                <input  className='flat-button' type='submit' value='Send'/>
-              </li>
-            </ul>
+              <ul>
+                <li className="half">
+                  <input type="text" name="name" placeholder="Name" required />
+                </li>
+                <li className="half">
+                  <input type="email" name="email" placeholder="Email" required />
+                </li>
+                <li>
+                  <input type="text" name="subject" placeholder="Subject" required />
+                </li>
+                <li>
+                  <textarea placeholder="Message" name="message" required />
+                </li>
+                <li>
+                  <input className="flat-button" type="submit" value="Send" />
+                </li>
+              </ul>
             </form>
           </div>
+        </div>
+
+        <div className="info-map">
+          Anurag Shrestha
+          <br />
+          301 Harvard DR SE, United States
+          <br />
+          <span>anuragshrestha448@gmail.com</span>
+        </div>
+
+        <div className="map-wrap">
+          <MapContainer center={[35.07703, -106.62114]} zoom={13}>
+            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            <Marker position={[35.07703, -106.62114]} />
+          </MapContainer>
         </div>
       </div>
       <Loader type="pacman" />
--- src/component/Sidebar/Sidebar.js
+++ src/component/Sidebar/Sidebar.js
@@ -1,41 +1,90 @@
-import { Link, NavLink } from 'react-router-dom';
-import './Sidebar.scss';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons';
-import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import './Sidebar.scss'
+import { useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faLinkedin,
+  faGithub,
+} from '@fortawesome/free-brands-svg-icons'
+import {
+  faHome,
+  faUser,
+  faEnvelope,
+  faBars,
+  faClose,
+} from '@fortawesome/free-solid-svg-icons'
+import { Link, NavLink } from 'react-router-dom'
 
+const Sidebar = () => {
+  const [showNav, setShowNav] = useState(false);
 
-const Sidebar = () => (
-   <div className='nav-bar' >
-     <Link className='logo' to ='/' >
-      <span className='name'>Anurag Shrestha</span>
-     </Link>
-     <nav>
-        <NavLink exact="true" activeclassname="active" to="/" >
-            <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+  return (
+    <div className="nav-bar">
+      <nav className={showNav ? 'mobile-show' : ''}>
+        <NavLink 
+          exact="true"
+          activeclassname="active"
+          to="/"
+          onClick={() => setShowNav(false)}>
+          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
         </NavLink>
-        <NavLink exact="true" activeclassname="active" className="about-link" to ="/about">
-            <FontAwesomeIcon icon={faUser} color='#4d4d4e' />
+        <NavLink 
+          activeclassname="active"
+          className="about-link"
+          to="/about"
+          onClick={() => setShowNav(false)}>
+          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
         </NavLink>
-        <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
-            <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e'/>
+        <NavLink
+          activeclassname="active"
+          className="contact-link"
+          to="/contact"
+          onClick={() => setShowNav(false)}
+        >
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
         </NavLink>
-     </nav>
-     <ul>
+        <FontAwesomeIcon 
+          onClick={() => setShowNav(false)}
+          icon={faClose}
+          color="#ffd700"
+          size="3x"
+          className='close-icon' />
+      </nav>
+      <ul>
         <li>
-            <a target='_blank' rel='noreferrer' href='https://www.linkedin.com/in/anuragshrestha19/' >
-                <FontAwesomeIcon icon={faLinkedin} color='#4d4d4e'/>
-            </a>
+          <a
+            href="https://www.linkedin.com/in/anuragshrestha19/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon
+              icon={faLinkedin}
+              color="#4d4d4e"
+              className="anchor-icon"
+            />
+          </a>
         </li>
         <li>
-            <a target='_blank' rel='noreferrer' href='https://github.com/anuragshrestha' >
-                <FontAwesomeIcon icon={faGithub}  color='#4d4d4e'/>
-            </a>
+          <a
+            href="https://github.com/anuragshrestha"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon
+              icon={faGithub}
+              color="#4d4d4e"
+              className="anchor-icon"
+            />
+          </a>
         </li>
-     </ul>
-   </div>
-)
+      </ul>
+      <FontAwesomeIcon 
+          onClick={() => setShowNav(true)}
+          icon={faBars}
+          color="#ffd700"
+          size="3x"
+          className='hamburger-icon' />
+    </div>
+  )
+}
 
-
-
-export default Sidebar;
+export default Sidebar
